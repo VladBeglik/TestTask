0dd65ff00a2d3fdebc2175b8dfca5243ee8449ba
VladBeglik <beglikvv@belhard.com>
2023-09-28 20:00:42 +0300

    Initial commit

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services;
+
+public class OrderService : IOrderService
+{
+    private readonly ApplicationDbContext _ctx;
+    private const int MIN_QUANTITY = 10;
+
+    public OrderService(ApplicationDbContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    public async Task<Order> GetOrder()
+    {
+        var maxPrice = await _ctx.Orders.MaxAsync(order => order.Price);
+        return (await _ctx.Orders.FirstOrDefaultAsync(order => order.Price == maxPrice))!;
+    }
+
+    public async Task<List<Order>> GetOrders()
+    {
+        return await _ctx.Orders.Where(_ => _.Quantity > MIN_QUANTITY).ToListAsync();
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services;
+
+public class UserService : IUserService
+{
+    private readonly ApplicationDbContext _ctx;
+
+    public UserService(ApplicationDbContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    public async Task<User> GetUser()
+    {
+        return await _ctx.Users
+            .Where(_ => _.Orders.Any())
+            .OrderByDescending(_ => _.Orders.Count())
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<List<User>> GetUsers()
+    {
+        return await _ctx.Users
+            .Where(_ => _.Status == UserStatus.Inactive)
+            .ToListAsync();
+    }
+}
